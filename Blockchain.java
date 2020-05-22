9501cc1a26de80370610866b2d57b1bfde7a402a
Shush1999 <65693500+Shush1999@users.noreply.github.com>
2020-05-22 15:27:54 -0700

    Add files via upload

--- Blockchain.java
+++ Blockchain.java
@@ -1,3 +1,5 @@
+package com.company;
+
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -6,15 +8,13 @@ import java.util.Date;
 public class Blockchain {
 
     private ArrayList<Block> blockchain = new ArrayList<>();
-    private int difficulty;
 
     public Blockchain(){
 
         //the zeroeth block is the genesis block which is added here
         this.blockchain.add(new Block(0, "0", 000,
-               new Data("Welcome to Blockchain!") , "000dc75a315c77a1f9c98fb6247d03dd18ac52632d7dc6a9920261d8109b37cf",
+                new Data("Welcome to Blockchain!") , "000dc75a315c77a1f9c98fb6247d03dd18ac52632d7dc6a9920261d8109b37cf",
                 604));
-        difficulty = 3;
     }
 
     public static void main (String[] args) throws NoSuchAlgorithmException {
@@ -36,16 +36,15 @@ public class Blockchain {
         return this.blockchain.get(this.blockchain.size() - 1);
     }
 
-
     //called in the main class or messages class
     public Block generateNextBlock(Data data) throws NoSuchAlgorithmException {
 
-        int currentIndex = (this.blockchain.size() - 1) + 1;
-        String previousHash = (this.blockchain.get(this.blockchain.size() - 1)).hash;
+        int currentIndex = this.getLatestBlock().index + 1;
+        String previousHash = this.getLatestBlock().hash;
         long currentTimeStamp = new Date().getTime();
         int currentNonce = 0; //not sure about this
-        String currentHashTemporary = "0";
 
+        String currentHashTemporary = "0";
         Block currentBlock = new Block(currentIndex, previousHash, currentTimeStamp, data, currentHashTemporary, currentNonce);
         String currentBlockHash = currentBlock.calculateBlockHash(currentBlock);
         currentBlock.hash = currentBlockHash;
@@ -54,22 +53,15 @@ public class Blockchain {
         return currentBlock;
     }
 
-    public boolean isValidHashDifficulty(Block block){
-
-        for(int i = 0; i < block.hash.length(); i++){
-
-            if(!(block.hash.substring(i,i+1).equals("0"))){
-
-                break;
-            }
-
-            return i >= this.difficulty;
-        }
-
-        return false;
-    }
-    
     public void accessBlock(String patientData) {
-    	
+
     }
+
+
+
+
+
+
+
+
 }
